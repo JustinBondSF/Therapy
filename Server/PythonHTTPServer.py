467df795ab90c686dd3893e32b1d4298e021d5d4
JustinBondSF <JustinBondSF@gmail.com>
2020-08-12 02:53:30 -0700

    initial commit for Io's website

---
+++ Server/PythonHTTPServer.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/index.html', methods=['GET', 'POST'])
+def form():
+    return render_template('index.html')
+
+
+@app.route('/index.html#contact', methods=['GET', 'POST'])
+def submit():
+    return render_template('/index.html', say=request.form['say'], to=request.form['to'])
+
+
+if __name__ == "__main__":
+    app.run()
