3528388952ca815a64c24587809be99362671cf1
JustinBondSF <JustinBondSF@gmail.com>
2020-08-21 20:50:34 -0700

    Styling

--- Server/PythonHTTPServer.py
+++ Server/PythonHTTPServer.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/index.html', methods=['GET', 'POST'])
+@app.route('/../index.html', methods=['GET', 'POST'])
 def form():
-    return render_template('index.html')
+    return render_template('/../index.html')
 
 
-@app.route('/index.html#contact', methods=['GET', 'POST'])
+@app.route('/../index.html#contact', methods=['GET', 'POST'])
 def submit():
     return render_template('/index.html', say=request.form['say'], to=request.form['to'])
 
